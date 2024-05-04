e9774bea86ec699b7609cb90590989f3b35424fc
Muh-Wale <muhammadolawale27@gmail.com>
2024-05-04 22:05:06 +0100

    Latest Commit

--- src/App.jsx
+++ src/App.jsx
@@ -34,8 +34,8 @@ function App() {
           <div
             className={
               activeMenu
-                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  bg-[#E5E5E5]'
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 bg-[#E5E5E5]'
             }
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,8 +14,8 @@ const Sidebar = () => {
         }
     };
 
-    const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
-    const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
+    const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-[#2D60FF]  text-xl font-medium m-2';
+    const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-xl font-medium text-[#B1B1B1] dark:text-gray-200 dark:hover:text-[#2d61ff9c] hover:bg-light-gray m-2';
 
     return (
         <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
@@ -46,8 +46,9 @@ const Sidebar = () => {
                             onClick={handleCloseSideBar} 
                             className={({ isActive }) => (isActive ? activeLink : normalLink)} 
                             exact={true}
-                        > 
-                            {item.icon} 
+                        >   <div className=' text-3xl'>
+                                {item.icon}
+                            </div> 
                             <span className="capitalize ">
                                 {item.name}
                             </span> 
