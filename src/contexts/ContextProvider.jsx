d9de93b1ed0dd1d99ad125cfa53c4d1a678afd2e
Muh-Wale <muhammadolawale27@gmail.com>
2024-05-06 01:59:44 +0100

    latest comit

--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -7,12 +7,13 @@ const StateContext = createContext();
 export const ContextProvider = ({ children }) => {
     const [screenSize, setScreenSize] = useState(undefined);
     const [activeMenu, setActiveMenu] = useState(true);
+    const [clickedText, setClickedText] = useState('');
 
 
 
     return (
         // eslint-disable-next-line react/jsx-no-constructed-context-values
-        <StateContext.Provider value={{ activeMenu, setActiveMenu, screenSize, setScreenSize }}>
+        <StateContext.Provider value={{ activeMenu, setActiveMenu, screenSize, setScreenSize, clickedText, setClickedText }}>
         {children}
         </StateContext.Provider>
     );
