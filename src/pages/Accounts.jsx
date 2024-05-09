4c0f42437f6c0ef37476b58ad136b8211ec2e79f
Muh-Wale <muhammadolawale27@gmail.com>
2024-05-09 01:02:13 +0100

    latest

--- src/pages/Accounts.jsx
+++ src/pages/Accounts.jsx
@@ -1,4 +1,5 @@
-import { AccountsMain, LastAcct, MyCard } from "../data/dummy"
+import { AccountsInvoice, AccountsMain, LastAcct, MyCard } from "../data/dummy"
+import  BarStatTres  from '../data/BarStatTres.png';
 
 
 const Accounts = () => {
@@ -21,48 +22,48 @@ const Accounts = () => {
                 </div>
             </section>
 
-            <section className=' p-5 flex justify-between flex-wrap mt-14 md:mt-0 bg-white md:bg-[#E5E5E5]'>
+            <section className=' p-5 flex justify-between flex-wrap mt-14 md:mt-0 bg-[#E5E5E5]'>
 
-                <div className='w-full xl:w-5/6 2xl:w-4/6'>
+                <div className='w-full xl:w-2/3'>
                     <div>
                         <h1 className=' font-semibold text-2xl'>Recent Transaction</h1>
                     </div>
 
-                    <div className='bg-white rounded-2xl my-3'>
+                    <div className='bg-white rounded-2xl my-3 w-fit'>
                         {LastAcct.map((item) =>(
-                            <div key={item.TextD} className='p-5 grid grid-cols-7 items-center'>
-                            <div className='text-2xl p-3 rounded-xl w-fit col-span-1' style={{ backgroundColor: item.IconBg, color: item.IconColor }}>
-                              {item.Icon}
+                            <div key={item.TextD} className='px-5 py-4 grid grid-cols-4 md:grid-cols-7  gap-7 items-center'>
+                                <div className='text-2xl p-3 rounded-2xl w-fit col-span-1' style={{ backgroundColor: item.IconBg, color: item.IconColor }}>
+                                    {item.Icon}
+                                </div>
+                            
+                                <div className='col-span-2'>
+                                    <h1 className='font-medium'>{item.ColAa}</h1>
+                                    <p className='text-gray-300 font-light'>{item.ColAb}</p>
+                                </div>
+                            
+                                <div className="md:flex flex-row gap-7 col-span-3 hidden">
+                                    <div>
+                                        <h1 className="text-gray-300 font-light">{item.TextA}</h1>
+                                    </div>
+                            
+                                    <div>
+                                        <h1 className="text-gray-300 font-light">{item.TextB}</h1>
+                                    </div>
+                            
+                                    <div>
+                                        <h1 className="text-gray-300 font-light">{item.TextC}</h1>
+                                    </div>
+                                </div>
+                            
+                                <div className='font-medium col-span-1' style={{ color: item.Textcolor }}>
+                                    {item.TextD}
+                                </div>
                             </div>
-                          
-                            <div className='col-span-2'>
-                              <h1 className='font-medium'>{item.ColAa}</h1>
-                              <p className='text-gray-300 font-light'>{item.ColAb}</p>
-                            </div>
-                          
-                            <div className="flex flex-row gap-5 col-span-3">
-                              <div>
-                                <h1 className="text-gray-300 font-light">{item.TextA}</h1>
-                              </div>
-                          
-                              <div>
-                                <h1 className="text-gray-300 font-light">{item.TextB}</h1>
-                              </div>
-                          
-                              <div>
-                                <h1 className="text-gray-300 font-light">{item.TextC}</h1>
-                              </div>
-                            </div>
-                          
-                            <div className='font-medium col-span-1' style={{ color: item.Textcolor }}>
-                              {item.TextD}
-                            </div>
-                          </div>
                         ))}
                     </div>
                 </div>
 
-                <div className='w-full'>
+                <div className=' w-full lg:w-1/3 max-w-[450px]'>
                     <div className='flex justify-between items-center'>
                         <h1 className=' font-semibold text-2xl'>My Cards</h1>
                         <p className=' font-semibold text-lg hover:underline cursor-pointer'>See All</p>
@@ -82,7 +83,7 @@ const Accounts = () => {
                                     </div>
                                 </div>
 
-                                <div className='flex gap-16 items-center leading-3 my-7 px-5'>
+                                <div className='flex gap-16 items-center leading-3 my-8 px-5'>
                                     <div className=''>
                                         <p className=' font-light text-sm uppercase'>{item.ColBa}</p>
                                         <p className='font-medium'>{item.ColBb}</p>
@@ -103,6 +104,44 @@ const Accounts = () => {
                     </div>
                 </div>
             </section>
+
+            <section className=' p-5 flex justify-between flex-wrap mt-14 md:mt-0 bg-white md:bg-[#E5E5E5]'>
+                <div className='w-full xl:w-5/6 2xl:w-4/6'>
+                    <div className='flex justify-between items-center'>
+                        <h1 className=' font-semibold text-2xl'>My Cards</h1>
+                    </div>
+
+                    <div className='my-3'>
+                        <img src={BarStatTres} alt="" />
+                    </div>
+                </div>
+
+                <div className='w-fit'>
+                    <div>
+                        <h1 className=' font-semibold text-2xl'>Invoices Sent</h1>
+                    </div>
+
+                    <div className='bg-white rounded-2xl my-3'>
+                        {AccountsInvoice.map((item) =>(
+                            <div key={item.TextA} className='px-8 flex gap-5 w-full items-center'>
+                                <div className='my-4 text-2xl p-3 rounded-full' style={{ backgroundColor: item.IconBg, color: item.Iconcolor }}>
+                                    {item.icon}
+                                </div>
+
+                                <div className='my-4'>
+                                    <h1 className=' font-medium'>{item.ColAa}</h1>
+                                    <p className=' text-gray-300 font-light'>{item.ColAb}</p>
+                                </div>
+
+                                <div className='my-4 font-medium' style={{ color: item.Textcolor }}>
+                                    {item.TextD}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
+
         </>
     )
 }
