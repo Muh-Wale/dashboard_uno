c14959b0008c48b5751669f5e1374ee27a2177c7
Muh-Wale <muhammadolawale27@gmail.com>
2024-06-04 19:33:33 +0100

    Last Change

--- src/pages/Loans.jsx
+++ src/pages/Loans.jsx
@@ -6,19 +6,36 @@ import LoanData from './LoanData';
 const Loans = () => {
     const { activeMenu } = useStateContext();
 
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
+
     return (
         <>
-            <section className={`${ activeMenu ? 'mt-[130px] md:mt-5' : 'mt-[130px] md:mt-20' } p-5`}>
-                <div className="flex justify-between items-center flex-wrap gap-x-0 gap-y-3 md:gap-5 p-2 md:p-5">
+            <section className={`${ activeMenu ? 'mt-[130px] md:mt-5' : 'mt-[130px] md:mt-20' } px-0 pt-5 md:p-5`}>
+                <div className="flex justify-between items-center flex-wrap gap-x-0 gap-y-3 md:gap-5 p-1 xlxs:p-2 md:p-0">
                     {LoansMain.map((item) => (
-                        <div key={item.TextB} className="flex justify-center gap-3 md:gap-5 p-5 rounded-2xl bg-white items-center max-w-[200px] 2xl:max-w-[250px] w-full">
-                            <div className='text-3xl p-3 rounded-full' style={{ backgroundColor: item.iconBg, color: item.iconColor }}>
+                        <div key={item.TextB} className="flex justify-center gap-3 md:gap-5 2xl:gap-12 p-2 md:p-5 rounded-2xl bg-white items-center min-w-[150px] max-w-[150px] xlxs:max-w-[160px] xss:max-w-[190px] md:max-w-[200px] 2xl:max-w-[230px] w-full">
+                            <div className=' text-xl md:text-3xl p-2 md:p-3 rounded-full' style={{ backgroundColor: item.iconBg, color: item.iconColor }}>
                                 {item.icon}
                             </div>
 
                             <div>
                                 <p className="text-[#718EBF]">{item.TextA}</p>
-                                <h1 className="text-xl font-medium">{item.TextB}</h1>
+                                <h1 className=" text-base md:text-xl font-semibold">{item.TextB}</h1>
                             </div>
                         </div>
                     ))}
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,26 +1,67 @@
 import React from 'react'
 import { useStateContext } from '../contexts/ContextProvider';
 import { ServicesList, ServicesMain } from '../data/dummy';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Services = () => {
     const { activeMenu } = useStateContext();
 
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
+
     return (
         <>
-            <section className={`${ activeMenu ? 'mt-[130px] md:mt-5' : 'mt-[130px] md:mt-20' } p-5`}>
-                <div className="flex justify-between flex-wrap gap-5">
-                    {ServicesMain.map((item) => (
-                        <div key={item.TextB} className=" grid grid-cols-3 gap-3 md:gap-5 2xl:gap-12 p-5 rounded-2xl bg-white items-center w-full max-w-[320px] col-span-1">
-                            <div className='text-3xl p-3 md:p-5 rounded-full overflow-hidden col-span-1 w-fit' style={{ backgroundColor: item.iconBg, color: item.iconColor }}>
-                                {item.icon}
-                            </div>
+            <section className={`${ activeMenu ? 'mt-[150px] md:mt-5' : 'mt-[150px] md:mt-20' } px-0 pt-5 md:p-5`}>
+                <div>
+                    {window.innerWidth < 540 ? (
+                        <Slider {...settings}>
+                            {ServicesMain.map((item) => (
+                                <div key={item.TextB}>
+                                    <div className=" flex gap-3 md:gap-5 2xl:gap-12 p-5 rounded-2xl bg-white items-center w-full max-w-[280px] mx-auto">
+                                        <div className='text-3xl p-3 md:p-5 rounded-full overflow-hidden w-fit' style={{ backgroundColor: item.iconBg, color: item.iconColor }}>
+                                            {item.icon}
+                                        </div>
 
-                            <div className=" col-span-2">
-                                <p className="text-[#718EBF]">{item.TextA}</p>
-                                <h1 className="text-xl font-semibold">{item.TextB}</h1>
-                            </div>
+                                        <div className="">
+                                            <p className="text-[#718EBF]">{item.TextA}</p>
+                                            <h1 className="text-lg font-semibold">{item.TextB}</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </Slider>
+                    ) : (
+                        <div className="flex justify-between items-center flex-wrap gap-x-0 gap-y-3 md:gap-5 p-1 xlxs:p-2 md:p-0">
+                            {ServicesMain.map((item) => (
+                                <div key={item.TextB} className=" grid grid-cols-3 gap-3 md:gap-5 2xl:gap-12 p-5 rounded-2xl bg-white items-center w-full max-w-[320px] col-span-1">
+                                    <div className='text-3xl p-3 md:p-5 rounded-full overflow-hidden col-span-1 w-fit' style={{ backgroundColor: item.iconBg, color: item.iconColor }}>
+                                        {item.icon}
+                                    </div>
+        
+                                    <div className=" col-span-2">
+                                        <p className="text-[#718EBF]">{item.TextA}</p>
+                                        <h1 className="text-xl font-semibold">{item.TextB}</h1>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    )}
                 </div>
             </section>
 
@@ -35,8 +76,8 @@ const Services = () => {
                         {ServicesList.map((item) =>(
                             <div key={item.ColAa} className=' p-2 md:px-5 md:py-4 flex justify-between gap-3 md:gap-2 items-center bg-white rounded-2xl my-3 w-full md:w-full'>
                                 
-                                <div className='flex gap-4'>
-                                    <div className='text-2xl p-3 rounded-2xl w-fit' style={{ backgroundColor: item.IconBg, color: item.IconColor }}>
+                                <div className='flex gap-4 items-center'>
+                                    <div className='text-2xl p-3 rounded-2xl w-fit flex items-center' style={{ backgroundColor: item.IconBg, color: item.IconColor }}>
                                         {item.Icon}
                                     </div>
                                 
